8b7a61e0ff691357e6650b2b8850292815cb2880
Brandon Carl <brandon.j.carl@gmail.com>
2016-01-29 08:21:42 -0500

    Renames server to main (given sub-servers)

--- index.js
+++ index.js
@@ -7,15 +7,15 @@
 
 module.exports = function() {
 
-  var app    = require("./lib/server"),
+  var server = require("./lib/main"),
       bs     = require("browser-sync").create(),
       config = require("./lib/config")();
 
   // Attach to the global namespace
   global.bs = bs;
 
-  // Start the app
-  app();
+  // Start the server
+  server();
 
   // Start browser sync and proxy
   bs.init({
