fe22566ebf08802b578c21ea69ec4980d2117733
Brandon Carl <brandon.j.carl@gmail.com>
2016-02-22 16:02:44 -0500

    Index: gets rid of extraneous modules

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
 
-/*
+/**
 
   index.js
 
   Entry point for sneakers.
 
-*/
+**/
 
 
-var fs    = require("fs"),
-    chalk = require("chalk"),
-    cli   = require("./cli/"),
-    Utils = require("./server/utils");
-
-
-// Ensure project has been initialized
-try {
-  fs.lstatSync(Utils.makePathAbsolute("sneakers.config.js"));
-  fs.lstatSync(Utils.makePathAbsolute("package.json"));
-} catch (err) {
-  console.log("You haven't initialized your project. Please run " + chalk.cyan("sneakers init") + " then try again.")
-  process.exit(1);
-}
+var cli = require("./cli/");
 
 // Set up globals
 require("./server/globals")();
